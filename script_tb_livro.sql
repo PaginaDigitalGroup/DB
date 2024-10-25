5de586d19388b9c283138414afe23b3ecb269460
Cleyfson Angelo <cleyfson12@outlook.com>
2024-10-24 21:20:20 -0300

    pequena modificacao no script de livro

--- script_tb_livro.sql
+++ script_tb_livro.sql
@@ -1,5 +1,11 @@
 select * from db_pagina_digital.tb_livro;
 
+ALTER TABLE db_pagina_digital.tb_livro DROP COLUMN autor_id;
+
+select * from db_pagina_digital.tb_autor;
+
+update tb_livro set foto = 'https://m.media-amazon.com/images/I/8147kKLLvOL._SY466_.jpg' where id = 7;
+
 ALTER TABLE db_pagina_digital.tb_livro
 MODIFY COLUMN descricao VARCHAR(5000);
 
@@ -8,7 +14,7 @@ MODIFY `numero_pagina` BIGINT NOT NULL DEFAULT 0;
 
 DESCRIBE `db_pagina_digital`.`tb_livro`;
 
-INSERT INTO db_pagina_digital.tb_livro (
+INSERT INTO tb_livro (
   titulo,
   editora,
   numero_paginas,
@@ -18,18 +24,29 @@ INSERT INTO db_pagina_digital.tb_livro (
   foto,
   tb_autor_id
 ) VALUES (
-  'Escuridão Total Sem Estrelas',
-  'Suma 1ª edição (26 março 2015)',
-  392,
-  'Na ausência da luz, o mundo assume formas sombrias, distorcidas, tenebrosas. Nesses quatro contos, Stephen King leva seus personagens a esses momentos de escuridão total, quando não existe nada ― bom senso, piedade, justiça ou estrelas ― para guiá-los. O primeiro deles inspirou o filme 1922 , da Netflix.
-Apesar de ter se tornado conhecido por seus romances, Stephen King também pode ser considerado um contista de sucesso, fazendo jus à fama de mestre do terror mesmo em poucas páginas. Em Escuridão total sem estrelas , King entrega quatro histórias curtas e aterrorizantes sobre o modo como lidamos com o mundo e como o mundo lida conosco.
-O livro começa com o conto que inspirou o filme da Netflix, 1922 , em que um agricultor convida seu filho para fazer parte de um plano terrível, visando garantir a posse das terras da família. Em seguida, no conto Gigante do volante , uma mulher estuprada por um estranho e deixada à beira da morte elabora uma vingança que a faz encarar um lado desconhecido de si mesma. Depois, em Extensão justa , um homem com câncer terminal faz um pacto com um estranho vendedor e tem que decidir até onde vale a pena ir para salvar a própria vida. Por fim, em Um bom casamento , uma mulher encontra uma caixa na garagem que pode dizer mais sobre seu marido do que os vinte anos que eles passaram juntos.
-Os contos de Escuridão total sem estrelas são poderosos e, cada um à sua maneira, profundamente chocantes ― The Guardian. Ao mergulhar nas histórias sinistras de Escuridão total sem estrelas , se deixando levar pela prosa vívida e brincalhona de Stephen King, o leitor pensa: caramba, esse cara está se divertindo. Um escritor que sente tanta alegria descarada no ato de contar uma história é uma raridade ― The New York Times',
+  'O Iluminado',
+  'Suma; 1ª edição (19 novembro 2012)',
+  464,
+  'O romance, magistralmente levado ao cinema por Stanley Kubrick, continua apaixonando (e aterrorizando) novas gerações de leitores. A luta assustadora entre dois mundos. Um menino e o desejo assassino de poderosas forças malignas. Uma família refém do mal. Nesta guerra sem testemunhas, vencerá o mais forte. Danny Torrance não é um menino comum. É capaz de ouvir pensamentos e transportar-se no tempo. Danny é iluminado. Será uma maldição ou uma bênção? A resposta pode estar guardada na imponência assustadora do hotel Overlook. Em O iluminado, quando Jack Torrance consegue o emprego de zelador no velho hotel, todos os problemas da família parecem estar solucionados. Não mais o desemprego e as noites de bebedeiras. Não mais o sofrimento da esposa, Wendy. Tranquilidade e ar puro para o pequeno Danny livrar-se das convulsões que assustam a família. Só que o Overlook não é um hotel comum. O tempo esqueceu-se de enterrar velhos ódios e de cicatrizar antigas feridas, e espíritos malignos ainda residem nos corredores. O hotel é uma chaga aberta de ressentimento e desejo de vingança. É uma sentença de morte. E somente os poderes de Danny podem fazer frente à disseminação do mal.',
   'Terror/Suspense',
   TRUE,
-  'https://www.literalmenteuai.com.br/wp-content/uploads/2017/10/Escurid%C3%A3o-total-sem-estrelas-stephen-King.jpg',
+  'https://m.media-amazon.com/images/I/8147kKLLvOL._SY466_.jpg',
   2
 );
+
+ALTER TABLE tb_livro 
+ADD CONSTRAINT fk_livro_autor 
+FOREIGN KEY (tb_autor_id) REFERENCES tb_autor(id);
+
+USE db_pagina_digital;
+
+select * from db_pagina_digital.tb_livro;
+
+ALTER TABLE db_pagina_digital.tb_livro
+DROP COLUMN autor_id;
+
+
+
 
 
 
